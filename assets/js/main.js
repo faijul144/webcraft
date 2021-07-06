4d89a70f98c587074e4472ca2e9601fe1c204462
Faijul Islam Fuad <f.faijulislam144@gmail.com>
2021-07-07 00:03:50 +0600

    2nd Commit Big screen done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,50 @@
 "use strict";
 
+// Smooth Scroll
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              // Checking if the target was focused
+              return false;
+            } else {
+              $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+              $target.focus(); // Set focus again
+            }
+          }
+        );
+      }
+    }
+  });
+
 // Set data-img as background style
-document.querySelectorAll("[data-img]").forEach(function (item) {
-  let bgImageSrc = item.getAttribute("data-img");
-  item.style.backgroundImage = `url(${bgImageSrc})`;
+$("[data-img]").each(function () {
+  let bgImageSrc = $(this).attr("data-img");
+  $(this).css("background-image", `url(${bgImageSrc})`);
 });
